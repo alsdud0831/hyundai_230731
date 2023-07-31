0bd14e680310622e627fae41349d29f5acb44487
Chansik Yun <chansigi@gmail.com>
2023-07-31 09:41:11 +0900

    .

--- 1_시작2.c
+++ 1_시작2.c
@@ -2,6 +2,41 @@
 // https://github.com/yoondev-classroom/hyundai_230731
 #include <stdio.h>
 
+// 1. Compile 과정
+//  - 전처리기
+//  #define
+//  #include
+//  #if
+//  ...
+// => 텍스트 처리
+
+// - 컴파일
+//   a.c  -> a.o / a.obj
+//   b.c  -> b.o / b.obj
+
+// - 링크
+//   a.obj + b.obj => a.exe / a.out
+
+// 2. 컴파일러
+// - Microsoft Visual C/C++ Compiler => MSVC
+// - GNU C/C++ Compiler => GCC
+// - LLVM Compiler => clang
+
+// 3. C 표준
+//  C89/C90 표준
+//  C99
+//  C11 - *
+//  C17
+//  C23
+
+// 참고 C++ 표준
+//  C++98/03 => 1.0
+//  C++11/14 => Modern C++
+//  C++17/20
+//  C++23
+
+// 4. 참고 문서 - https://devdocs.io
+
 int main(void)
 {
     return 0;
